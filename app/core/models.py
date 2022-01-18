0bf6e778462b56294ccfc2c2142b2339c72bad4a
VPuosk <vesa.puoskari@gmail.com>
2022-01-18 22:15:45 +0200

    added recipe api features

--- app/core/models.py
+++ app/core/models.py
@@ -60,3 +60,21 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField(Ingredient)
+    tags = models.ManyToManyField(Tag)
+    # could be 'Tag' in which case the depency order would not matter
+
+    def __str__(self):
+        return self.title
